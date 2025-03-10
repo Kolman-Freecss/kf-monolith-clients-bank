020755c9b04e16f1de7edd18055c6dc504b6a2b3
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2025-03-10 20:45:38 +0100

    Logs

--- src/main/java/org/kolmanfreecss/kf_monolith_clients_bank/KfMonolithClientsBankApplication.java
+++ src/main/java/org/kolmanfreecss/kf_monolith_clients_bank/KfMonolithClientsBankApplication.java
@@ -2,7 +2,12 @@ package org.kolmanfreecss.kf_monolith_clients_bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class KfMonolithClientsBankApplication {
 
@@ -10,4 +15,19 @@ public class KfMonolithClientsBankApplication {
         SpringApplication.run(KfMonolithClientsBankApplication.class, args);
     }
 
+    @EventListener(ContextRefreshedEvent.class)
+    public void testLogging() {
+        log.trace("This is a TRACE level message - Very detailed information");
+        log.debug("This is a DEBUG level message - Detailed information");
+        log.info("This is an INFO level message - General information");
+        log.warn("This is a WARN level message - Warning");
+        log.error("This is an ERROR level message - Application error");
+
+        // Exception example
+        try {
+            throw new RuntimeException("This is a test exception");
+        } catch (Exception e) {
+            log.error("This is an ERROR message with stack trace", e);
+        }
+    }
 }
