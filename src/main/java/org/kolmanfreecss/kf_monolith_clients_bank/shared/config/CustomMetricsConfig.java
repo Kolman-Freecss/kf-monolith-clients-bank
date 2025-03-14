f953ff73e6311794fe8ce8b8457542d662171d52
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2025-03-14 18:39:02 +0100

    Update changes

--- src/main/java/org/kolmanfreecss/kf_monolith_clients_bank/shared/config/CustomMetricsConfig.java
+++ src/main/java/org/kolmanfreecss/kf_monolith_clients_bank/shared/config/CustomMetricsConfig.java
@@ -1,11 +1,11 @@
 package org.kolmanfreecss.kf_monolith_clients_bank.shared.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.Value;
 
 @Configuration
 public class CustomMetricsConfig {
